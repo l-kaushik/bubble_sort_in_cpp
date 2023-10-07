a6952587ba2c13f50894e4acf46b2395025eca57
l-kaushhik <contact.lokeshkaushik@gmail.com>
2023-10-07 13:31:08 +0530

    Added function to print array

--- main.cpp
+++ main.cpp
@@ -2,7 +2,18 @@
 
 using namespace std;
 
+
+void printArray(int a[], int size){
+    for(int i = 0; i<size; i++){
+        cout<<a[i]<<"\t";
+    }
+    cout<<endl;
+}
+
 int main()
 {
-    cout<<"Hello world"<<endl;
+    int a[] = {5,3,2,6,9,3,-1,0};
+    int size = sizeof(a)/sizeof(a[0]);
+    printArray(a, size);
+
 }
